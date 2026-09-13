9203bef4bacdc88c45901a9f7e8d0cd11e2d7692
gengzeyu <2695917656@qq.com>
2026-09-13 16:21:35 +0800

    logs: 优化prompt后plantocode生成的双机器人code_plan + 执行产物

--- logs/Slice_the_tomato_plans_09-09-2026-09-53-33/executable_plan.py
+++ logs/Slice_the_tomato_plans_09-09-2026-09-53-33/executable_plan.py
@@ -578,15 +578,18 @@ def ThrowObject(robot, sw_obj):
 def slice_tomato(robots):
     GoToObject(robots[0], 'Knife')
     PickupObject(robots[0], 'Knife')
+    GoToObject(robots[1], 'Tomato')
+    PickupObject(robots[1], 'Tomato')
     GoToObject(robots[0], 'Tomato')
-    PickupObject(robots[0], 'Tomato')
     SliceObject(robots[0], 'Tomato')
-    PutObject(robots[0], 'Tomato', 'CounterTop')
+    GoToObject(robots[1], 'CounterTop')
+    PutObject(robots[1], 'Tomato', 'CounterTop')
+    GoToObject(robots[0], 'CounterTop')
     PutObject(robots[0], 'Knife', 'CounterTop')
 
-task_thread = threading.Thread(target=slice_tomato, args=(robots,))
-task_thread.start()
-task_thread.join()
+task1_thread = threading.Thread(target=slice_tomato, args=(robots,))
+task1_thread.start()
+task1_thread.join()
 action_queue.append({'action':'Done'})
 task_over = True
 time.sleep(5)
